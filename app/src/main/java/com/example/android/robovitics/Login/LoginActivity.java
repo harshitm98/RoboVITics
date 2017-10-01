5ea8ae951cf2f9cda644fc8dceb468c814724bb5
harshitm98 <harshitmah98@gmail.com>
2017-10-01 11:59:31 +0530

    Error: Authentification failed!

--- app/src/main/java/com/example/android/robovitics/Login/LoginActivity.java
+++ app/src/main/java/com/example/android/robovitics/Login/LoginActivity.java
@@ -1,15 +1,77 @@
 package com.example.android.robovitics.Login;
 
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.robovitics.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final String TAG = "LoginActivity";
+
+    private TextView emailId, password, newAccountText;
+    private FloatingActionButton signInButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInButton = (FloatingActionButton)findViewById(R.id.sign_in);
+        emailId = (TextView)findViewById(R.id.email_id);
+        password = (TextView)findViewById(R.id.password);
+        newAccountText = (TextView)findViewById(R.id.text_new_user);
+        newAccountText.setPaintFlags(newAccountText.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewAccount();
+            }
+        });
+
+    }
+
+    public void createNewAccount() {
+        String mEmail, mPassword;
+
+        mEmail = emailId.getText().toString().trim();
+        mPassword = password.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(mEmail, mPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Successfully created new account.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/android/robovitics/Splash.java
+++ app/src/main/java/com/example/android/robovitics/Splash.java
@@ -5,21 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.android.robovitics.Login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Splash extends Activity {
 
-    private final int SPLASH_LENGTH = 2000;
+    private final int SPLASH_LENGTH = 3000;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mAuth = FirebaseAuth.getInstance();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                FirebaseUser user = mAuth.getCurrentUser();
+
+                if(user == null){
+                    Intent intent = new Intent(Splash.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+
             }
         }, SPLASH_LENGTH);
     }
