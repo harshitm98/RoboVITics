359f2a01989f24bfe26a51948935c3493311717b
harshitm98 <harshitmah98@gmail.com>
2018-05-12 01:01:10 +0530

    Profile done!

--- app/src/main/java/com/example/android/robovitics/ClubMembers/ClubMemberAdapter.java
+++ app/src/main/java/com/example/android/robovitics/ClubMembers/ClubMemberAdapter.java
@@ -1,14 +1,19 @@
 package com.example.android.robovitics.ClubMembers;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +25,15 @@ import java.util.List;
  * Created by Harshit Maheshwari on 20-10-2017.
  */
 
-public class ClubMemberAdapter extends ArrayAdapter<ClubMembersObject> {
+public class ClubMemberAdapter extends ArrayAdapter<ClubMembersObject>{
+
+    private static final String TAG = "ClubMemberAdapter";
+
+    private Context context;
+
     public ClubMemberAdapter(@NonNull Context context, @NonNull List<ClubMembersObject> objects) {
         super(context,0,0, objects);
+        this.context = context;
     }
 
     @NonNull
@@ -30,26 +41,49 @@ public class ClubMemberAdapter extends ArrayAdapter<ClubMembersObject> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listitem = convertView;
 
+
         if(listitem == null){
             listitem = LayoutInflater.from(getContext()).inflate(R.layout.list_item_club_member,parent,false);
         }
 
-        ClubMembersObject object = getItem(position);
+        final ClubMembersObject object = getItem(position);
 
         final TextView name = (TextView)listitem.findViewById(R.id.list_name);
         name.setText(object.getsName());
 
-//        name.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getContext(),name.getText(),Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        final TextView registration = listitem.findViewById(R.id.reg_number_list);
+        registration.setText(object.getRegistrationNumber());
 
-        /*
-        TODO: Insert imageView and display the image
-         */
+        final TextView room = listitem.findViewById(R.id.room_number_list);
+        room.setText(object.getRoomNumber());
 
+        final TextView phone = listitem.findViewById(R.id.list_phone);
+        phone.setText(object.getPhoneNumber());
+
+        final Button callButton = listitem.findViewById(R.id.list_call_button);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", object.getPhoneNumber(), null));
+                context.startActivity(intent);
+            }
+        });
+
+        final CardView cardView = listitem.findViewById(R.id.card_view);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(),ProfileActivity.class);
+                intent.putExtra("name",object.getsName());
+                intent.putExtra("reg",object.getRegistrationNumber());
+                intent.putExtra("phone_number",object.getPhoneNumber());
+                intent.putExtra("room_number",object.getRoomNumber());
+                intent.putExtra("skills",object.getSkills());
+                intent.putExtra("email",object.getEmailId());
+                intent.putExtra("uid",object.getUID());
+                context.startActivity(intent);
+            }
+        });
         return listitem;
     }
 }
--- app/src/main/java/com/example/android/robovitics/ClubMembers/FragmentClubMembers.java
+++ app/src/main/java/com/example/android/robovitics/ClubMembers/FragmentClubMembers.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.android.robovitics.R;
 import com.google.firebase.database.ChildEventListener;
